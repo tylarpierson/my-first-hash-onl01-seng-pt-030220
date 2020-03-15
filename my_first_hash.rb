b7d5802fdd6879e0689ad6468dbaa152f57f74ac
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 23:15:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  person = {
+    "name" => "Tylar",
+    "age" => "24"
+  }
 end
 
 
@@ -9,7 +11,10 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  the_manifest = {}
+  the_manifest["whale bone corsets"] = "5"
+  the_manifest["porcelain vases"] = "2"
+  the_manifest["oil paintings"] = "3"
 
 end
 
